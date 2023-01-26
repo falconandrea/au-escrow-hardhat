fd36141ffe8fe735daf07fc20993d60fd055137d
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2023-01-26 18:36:46 +0100

    feat: add dotenv and update hardhat config for goerli connection

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,15 @@
 require('@nomicfoundation/hardhat-toolbox')
+require('dotenv').config()
 
 module.exports = {
   solidity: '0.8.17',
   paths: {
     artifacts: './app/src/artifacts'
+  },
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_NETWORK_URL,
+      accounts: [process.env.GOERLI_PRIVATE_KEY]
+    }
   }
 }
